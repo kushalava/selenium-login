573d89ada1cd00a96bb49f61c10083e5d1b16e0a
kushalava <kushalava@gmail.com>
2018-06-21 20:10:13 -0700

    login.java
    This script will validate Login page and contributed by Sai, Pratap.
    I added new test case.

---
+++ login.java
@@ -0,0 +1,28 @@
+package com.myselenium.tutorials;
+
+import org.testng.annotations.Test;
+
+public class TestngAnnotation {
+	// test case 1
+	@Test(priority = 2)
+	public void testCase1() {
+		System.out.println("in test case 1");
+	}
+
+	// test case 2
+	@Test(priority = 3)
+	public void testCase2() {
+		System.out.println("in test case 2");
+	}
+	// test case 2
+	@Test(priority = 4)
+	public void testCase4() {
+		System.out.println("in test case 2");
+	}
+	// test case 2
+	@Test(priority = 1;TestRun=enabled;Timeout=20)
+	public void testCase3() {
+		System.out.println("in test case 2");
+	}
+
+}
